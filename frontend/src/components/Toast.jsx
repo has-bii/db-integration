6b75006a8ddc65cb05fab363231fdab75db02ed3
has-bii <hasbii.rofficial@gmail.com>
2023-09-12 11:02:45 +0300

    Multiple Toast

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -3,23 +3,32 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-function Toast({ status, message, setToast }) {
+function Toast({ messages, delToast }) {
   return (
     <div className="toast-container">
-      <div className={`toast ${status ? "green" : "red"}`}>
-        <span>{message}</span>
-        <button className="appearance-none" onClick={() => setToast(null)}>
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-      </div>
+      {messages.map((message, i) => (
+        <div key={i} className={`toast ${message.status ? "green" : "red"}`}>
+          <span>{message.message}</span>
+          <button
+            className="appearance-none"
+            onClick={() => delToast(message.id)}
+          >
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 Toast.propTypes = {
-  status: PropTypes.bool.isRequired,
-  message: PropTypes.string.isRequired,
-  setToast: PropTypes.func.isRequired,
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string.isRequired,
+      status: PropTypes.bool.isRequired,
+    })
+  ),
+  delToast: PropTypes.func.isRequired,
 };
 
 export default Toast;
--- frontend/src/components/ToastProvider.jsx
+++ frontend/src/components/ToastProvider.jsx
@@ -4,24 +4,30 @@ import Toast from "./Toast";
 const ToastContext = createContext();
 
 export const ToastProvider = ({ children }) => {
-  const [data, setData] = useState(null);
-  const [delTimeout, setDelTimeout] = useState(null);
+  const [data, setData] = useState([]);
+  const [to, setTo] = useState(null);
 
   useEffect(() => {
-    if (data) {
-      setTimeout(() => setData(null), 3000);
+    if (data.length > 0) {
+      if (to) clearTimeout(to);
+
+      setTo(setTimeout(() => setData(data.slice(1, data.length)), 2000));
     }
   }, [data]);
 
   function pushToast(status, message) {
-    setData({ status, message });
+    const id = data.length;
+
+    setData((prev) => [...prev, { status, message, id }]);
+  }
+
+  function delToast(id) {
+    setData(data.filter((dt) => dt.id !== id));
   }
 
   return (
     <ToastContext.Provider value={{ pushToast }}>
-      {data && (
-        <Toast message={data.message} status={data.status} setToast={setData} />
-      )}
+      {data && <Toast messages={data} delToast={delToast} />}
       {children}
     </ToastContext.Provider>
   );
