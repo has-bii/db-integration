629480be44090fd4647ed91f7c79184664f4a1e9
has-bii <hasbii.rofficial@gmail.com>
2023-09-14 16:29:20 +0300

    handling new API to handle tasks

--- API/backup/index.js
+++ API/backup/index.js
@@ -5,98 +5,199 @@ const { main } = require("../../app/app");
 const intervals = [];
 
 router.post("/start", (req, res) => {
-  const reqIntervals = req.body.intervals;
+  try {
+    const reqIntervals = req.body.intervals;
 
-  if (!reqIntervals) {
-    return res.status(400).json({ message: "Intervals property is required!" });
-  }
-
-  // Stop and clear existing cron jobs
-  intervals.forEach((interval) => {
-    if (interval.status === true && interval.cronJob) {
-      interval.cronJob.stop();
-      interval.cronJob = null;
-    }
-  });
-
-  intervals.length = 0; // Clear the intervals array
-
-  // Modify and add new intervals
-  reqIntervals.forEach((reqInt) => {
-    if (reqInt.type === "hour") {
-      reqInt.value = Math.max(1, Math.min(24, reqInt.value));
-    } else if (reqInt.type === "minute") {
-      reqInt.value = Math.max(1, Math.min(60, reqInt.value));
+    if (!reqIntervals) {
+      return res
+        .status(400)
+        .json({ message: "Intervals property is required!" });
     }
 
-    reqInt.status = !!reqInt.status; // Ensure status is a boolean
-    intervals.push(reqInt);
-  });
-
-  // Remove deleted intervals
-  intervals.slice().forEach((int, index) => {
-    const exists = reqIntervals.some(
-      (reqInt) => reqInt.value === int.value && reqInt.type === int.type
-    );
-
-    if (!exists) {
-      intervals.splice(index, 1);
-      if (int.cronJob) {
-        int.cronJob.stop();
+    // Stop and clear existing cron jobs
+    intervals.forEach((interval) => {
+      if (interval.status === true && interval.cronJob) {
+        interval.cronJob.stop();
+        interval.cronJob = null;
       }
-    }
-  });
+    });
 
-  // Start new cron jobs
-  intervals.forEach((int) => {
-    if (int.status === true) {
-      int.cronJob = main(int.value, int.type);
-    }
-  });
+    intervals.length = 0; // Clear the intervals array
 
-  res.status(200).json({
-    message: "Backup started",
-    intervals: intervals.map((int) => ({
-      value: int.value,
-      type: int.type,
-      status: int.status,
-    })),
-  });
+    // Modify and add new intervals
+    reqIntervals.forEach((reqInt) => {
+      if (reqInt.type === "hour") {
+        reqInt.value = Math.max(1, Math.min(24, reqInt.value));
+      } else if (reqInt.type === "minute") {
+        reqInt.value = Math.max(1, Math.min(60, reqInt.value));
+      }
+
+      reqInt.status = !!reqInt.status; // Ensure status is a boolean
+      intervals.push(reqInt);
+    });
+
+    // Remove deleted intervals
+    intervals.slice().forEach((int, index) => {
+      const exists = reqIntervals.some(
+        (reqInt) => reqInt.value === int.value && reqInt.type === int.type
+      );
+
+      if (!exists) {
+        intervals.splice(index, 1);
+        if (int.cronJob) {
+          int.cronJob.stop();
+        }
+      }
+    });
+
+    // Start new cron jobs
+    intervals.forEach((int) => {
+      if (int.status === true) {
+        int.cronJob = main(int.value, int.type);
+      }
+    });
+
+    res.status(200).json({
+      message: `${
+        intervals.filter((int) => int.status).length
+      } tasks are running`,
+      intervals: intervals.map((int) => ({
+        value: int.value,
+        type: int.type,
+        status: int.status,
+      })),
+    });
+  } catch (error) {
+    console.error("Error has occurred while starting task: ", error);
+    res.status(400).json({ message: error });
+  }
 });
 
-router.get("/stop", (req, res) => {
-  if (!backup) {
-    res.status(200).json({ message: "Backup is not running", timestamp });
-  } else {
-    backup.stop();
+router.post("/start-all", (req, res) => {
+  try {
+    const reqIntervals = req.body.intervals;
 
-    backup = null;
+    if (!reqIntervals) {
+      return res
+        .status(400)
+        .json({ message: "Intervals property is required!" });
+    }
 
-    timestamp = new Date();
+    // Stop and clear existing cron jobs
+    intervals.forEach((interval) => {
+      if (interval.status === true && interval.cronJob) {
+        interval.cronJob.stop();
+        interval.cronJob = null;
+      }
+    });
 
-    console.log("\n\nMAIN BACKUP STOPPED\n\n");
+    intervals.length = 0; // Clear the intervals array
 
-    res.status(200).json({ message: "Backup is stopped", timestamp });
+    // Modify and add new intervals
+    reqIntervals.forEach((reqInt) => {
+      if (reqInt.type === "hour") {
+        reqInt.value = Math.max(1, Math.min(24, reqInt.value));
+      } else if (reqInt.type === "minute") {
+        reqInt.value = Math.max(1, Math.min(60, reqInt.value));
+      }
+
+      reqInt.status = true; // Change all statuses to be true
+      intervals.push(reqInt);
+    });
+
+    // Remove deleted intervals
+    intervals.slice().forEach((int, index) => {
+      const exists = reqIntervals.some(
+        (reqInt) => reqInt.value === int.value && reqInt.type === int.type
+      );
+
+      if (!exists) {
+        intervals.splice(index, 1);
+        if (int.cronJob) {
+          int.cronJob.stop();
+        }
+      }
+    });
+
+    // Start new cron jobs
+    intervals.forEach((int) => {
+      if (int.status === true) {
+        int.cronJob = main(int.value, int.type);
+      }
+    });
+
+    res.status(200).json({
+      message: `${
+        intervals.filter((int) => int.status).length
+      } tasks are running`,
+      intervals: intervals.map((int) => ({
+        value: int.value,
+        type: int.type,
+        status: int.status,
+      })),
+    });
+  } catch (error) {
+    console.error("Error has occurred while starting task: ", error);
+    res.status(400).json({ message: error });
+  }
+});
+
+router.get("/stop-all", (req, res) => {
+  try {
+    // Stop and clear existing cron jobs
+    intervals.forEach((interval) => {
+      if (interval.cronJob) {
+        interval.cronJob.stop();
+        interval.cronJob = null;
+      }
+      interval.status = false;
+    });
+
+    res.status(200).json({
+      message: "All tasks has been stopped",
+      intervals: intervals.map((int) => {
+        return { value: int.value, type: int.type, status: int.status };
+      }),
+    });
+  } catch (error) {
+    console.error("Error has occurred while stopping tasks: ", error);
+    res.status(400).json({ message: error });
   }
 });
 
 router.post("/restart", (req, res) => {
-  if (req.body.timeInterval) interval = req.body.timeInterval;
+  try {
+    // Stop and clear existing cron jobs
+    intervals.forEach((interval) => {
+      if (interval.status === true && interval.cronJob) {
+        interval.cronJob.stop();
+        interval.cronJob = null;
+      }
+    });
 
-  if (backup) backup.stop();
+    // Start new cron jobs
+    intervals.forEach((int) => {
+      if (int.status === true) {
+        int.cronJob = main(int.value, int.type);
+      }
+    });
 
-  backup = main(interval);
-
-  timestamp = new Date();
-
-  console.log(`\n\nMAIN BACKUP RESTARTED EVERY ${interval} MINS \n\n`);
-
-  res.status(200).json({ message: "Backup restarted", timestamp, interval });
+    res.status(200).json({
+      message: "Backup restarted",
+      intervals: intervals.map((int) => ({
+        value: int.value,
+        type: int.type,
+        status: int.status,
+      })),
+    });
+  } catch (error) {
+    console.error("Error has occurred while restarting tasks: ", error);
+    res.status(400).json({ message: error });
+  }
 });
 
 router.get("/isRunning", (req, res) => {
   res.status(200).json({
-    message: "Backup is not running.",
     intervals: intervals.map((int) => {
       return { value: int.value, type: int.type, status: int.status };
     }),
