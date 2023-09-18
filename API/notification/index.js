a52a42d209cc4defbb55b48a3550b4a179ef3cee
has-bii <hasbii.rofficial@gmail.com>
2023-09-18 17:01:07 +0300

    create deleting and fetching ErrorJSON API

--- API/notification/index.js
+++ API/notification/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const { clearErrorJSON, readErrors } = require("../../lib/ErrorHandler");
+const {
+  clearErrorJSON,
+  readErrors,
+  deleteErrorJSON,
+} = require("../../lib/ErrorHandler");
+
+router.get("/", (req, res) => {
+  try {
+    const FILE_PATH = process.env.ERROR_JSON_FILE || "error/error.json";
+
+    const data = JSON.parse(fs.readFileSync(FILE_PATH, "utf-8"));
+
+    res
+      .status(200)
+      .json({ message: "Errors JSON has been fetched successfully.", data });
+  } catch (error) {
+    res.status(400).json({ message: "Error while fetching errors JSON" });
+  }
+});
 
 router.get("/clear", (req, res) => {
   try {
@@ -20,6 +38,18 @@ router.get("/read", (req, res) => {
     res.status(200).json({ message: "Notifications have been read." });
   } catch (error) {
     res.status(400).json({ message: "Error while reading notifications!" });
+  }
+});
+
+router.post("/delete", (req, res) => {
+  try {
+    const { data } = req.body;
+
+    deleteErrorJSON(data);
+
+    res.status(200).json({ message: "Notification has been deleted." });
+  } catch (error) {
+    res.status(400).json({ message: "Error while deleting the notification!" });
   }
 });
 
